c1b3314df70504312a229de902c411505bf070b6
Doom4eg101 <rostikbalagurak@gmail.com>
2013-10-25 19:11:17 +0300

    rollbackQuietly method fix

--- src/dao/JdbcUtils.java
+++ src/dao/JdbcUtils.java
@@ -4,15 +4,18 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class JdbcUtils {
-	
-	public static void rollbackQuietly (Connection conn) {
-		try {
-			conn.rollback();
-		} catch (SQLException e) {
-			// ignore
+
+	public static void rollbackQuietly(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.rollback();
+			} catch (SQLException e) {
+				// ignore
+			}
 		}
+
 	}
-	
+
 	public static void closeQuietly(AutoCloseable obj) {
 		if (obj != null) {
 			try {
